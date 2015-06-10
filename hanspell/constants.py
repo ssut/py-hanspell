ca2487b857d77c900fe25f7843fc93aae1843f6f
ssut <ssut@ssut.me>
2015-06-11 02:49:56 +0900

    라이브러리 작성

---
+++ hanspell/constants.py
@@ -0,0 +1,7 @@
+base_url = 'https://m.search.naver.com/p/csearch/dcontent/spellchecker.nhn'
+
+class CheckResult:
+    PASSED = 0
+    WRONG_SPELLING = 1
+    WRONG_SPACING = 2
+    AMBIGUOUS = 3
---
+++ hanspell/spell_checker.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+Python용 한글 맞춤법 검사 모듈
+"""
+import requests
+import json
+import time
+import sys
+from collections import OrderedDict
+import xml.etree.ElementTree as ET
+
+from . import __version__
+from .response import Checked
+from .constants import base_url
+from .constants import CheckResult
+
+_agent = requests.Session()
+PY3 = sys.version_info[0] == 3
+
+def _remove_tags(text):
+    text = u'<content>{}</content>'.format(text)
+    if not PY3:
+        text = text.encode('utf-8')
+    result = ''.join(ET.fromstring(text).itertext())
+
+    return result
+
+def check(text):
+    """
+    매개변수로 입력받은 한글 문장의 맞춤법을 체크합니다.
+    """
+    if isinstance(text, list):
+        result = []
+        for item in tesxt:
+            checked = check(item)
+            result.append(checked)
+        return result
+
+    # 최대 500자까지 가능.
+    if len(text) > 500:
+        return Checked(result=False)
+
+    payload = {
+        '_callback': '',
+        'q': text,
+    }
+    start_time = time.time()
+    r = _agent.get(base_url, params=payload)
+    passed_time = time.time() - start_time
+    r = r.text[1:-2]  # ({json}); -> {json}
+    data = json.loads(r)
+    html = data['message']['result']['html']
+    result = {
+        'result': True,
+        'original': text,
+        'checked': _remove_tags(html),
+        'errors': data['message']['result']['errata_count'],
+        'time': passed_time,
+        'words': OrderedDict(),
+    }
+
+    # 띄어쓰기로 구분하기 위해 태그는 일단 보기쉽게 바꿔둠.
+    # ElementTree의 iter()를 써서 더 좋게 할 수 있는 방법이 있지만
+    # 이 짧은 코드에 굳이 그렇게 할 필요성이 없으므로 일단 문자열을 치환하는 방법으로 작성.
+    html = html.replace('<span class=\'re_green\'>', '<green>') \
+               .replace('<span class=\'re_red\'>', '<red>') \
+               .replace('<span class=\'re_purple\'>', '<purple>') \
+               .replace('</span>', '<end>')
+    items = html.split(' ')
+    words = []
+    tmp = ''
+    for word in items:
+        if tmp == '' and word[:1] == '<':
+            pos = word.find('>') + 1
+            tmp = word[:pos]
+        elif tmp != '':
+            word = u'{}{}'.format(tmp, word)
+        
+        if word[-5:] == '<end>':
+            word = word.replace('<end>', '')
+            tmp = ''
+
+        words.append(word)
+
+    for word in words:
+        check_result = CheckResult.PASSED
+        if word[:5] == '<red>':
+            check_result = CheckResult.WRONG_SPELLING
+            word = word.replace('<red>', '')
+        elif word[:7] == '<green>':
+            check_result = CheckResult.WRONG_SPACING
+            word = word.replace('<green>', '')
+        elif word[:8] == '<purple>':
+            check_result = CheckResult.AMBIGUOUS
+            word = word.replace('<purple>', '')
+
+        result['words'][word] = check_result
+
+    return result

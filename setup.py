c493fbd6c6c10f5438cef2939917ef53d77b90f4
hs0907j <37132917+hs0907j@users.noreply.github.com>
2018-11-21 13:57:54 +0900

    Update setup.py
    because Pip version was updated, we need to take care of that.

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+    
 from setuptools import setup, find_packages
 
 def install():
